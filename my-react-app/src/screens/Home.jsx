bbb465457daa6f0c78ef1f08352e56e6d3e78d0a
Endrit02 <endritdrenica2002@gmail.com>
2025-10-02 21:25:31 +0200

    Search

--- my-react-app/src/screens/Home.jsx
+++ my-react-app/src/screens/Home.jsx
@@ -1,7 +1,12 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useMemo } from 'react';
 
 const Home = () => {
     const [users, setUsers] = useState(null);
+    const [searchValue, setSearchValue] = useState('');
+
+    const handleSearchChange = (e) => {
+        setSearchValue(e.target.value);
+    }
 
     const fetchUsers = async () => {
         try {
@@ -14,15 +19,31 @@ const Home = () => {
         }
     }
 
-    console.log('users', users);
+    const filteredUsers = useMemo(() => {
+        if (searchValue !== null || searchValue !== '') {
+            return users ? users?.filter(u => u.name.toLowerCase().includes(searchValue.toLowerCase()) || u.email.toLowerCase().includes(searchValue.toLowerCase().toLowerCase())) : [];
+        }
+    }, [searchValue, users])
 
     useEffect(() => {
         fetchUsers();
     }, [])
 
     return (
-        <div className="bg-red-500 w-[300px] h-[300px]">
-            asdf
+        <div className="wrapper">
+            <h1>Users List</h1>
+            <div className="users-search">
+                <input type="text" placeholder='Search' value={searchValue} onChange={handleSearchChange} />
+            </div>
+            <div className="users-wrapper">
+                {filteredUsers?.map(user => (
+                    <div className="user">
+                        <p>Name: {user.name}</p>
+                        <p>Email: {user.email}</p>
+                        <p>Company: {user.company.name}</p>
+                    </div>
+                ))}
+            </div>
         </div>
     )
 }
